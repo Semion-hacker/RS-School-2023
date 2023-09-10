7a58c9185f43cdf1c26876fcd2a57b6aec2489a6
Semion-hacker <obucheniekurs7@gmail.com>
2023-09-10 15:34:35 +0300

    feat: add carousel and add slider

---
+++ library/scripts/just_a_script.js
@@ -0,0 +1,191 @@
+const firstBook = document.querySelector('.first-book')
+const secondBook = document.querySelector('.second-book')
+const thirdBook = document.querySelector('.third-book')
+const fourthBook = document.querySelector('.fourth-book')
+
+const fifthBook = document.querySelector('.fifth-book')
+const sixthBook = document.querySelector('.sixth-book')
+const seventhBook = document.querySelector('.seventh-book')
+const eighthBook = document.querySelector('.eighth-book')
+
+const ninthBook = document.querySelector('.ninth-book')
+const tenthBook = document.querySelector('.tenth-book')
+const elevenBook = document.querySelector('.eleven-book')
+const twelfthBook = document.querySelector('.twelfth-book')
+
+const thirteenthBook = document.querySelector('.thirteenth-book')
+const fourteenthBook = document.querySelector('.fourteenth-book')
+const fifteenthBook = document.querySelector('.fifteenth-book')
+const sixteenthBook = document.querySelector('.sixteenth-book')
+
+document.querySelector('.label1').addEventListener('click', function(){
+    firstBook.style.visibility = 'visible';
+    secondBook.style.visibility = 'visible';
+    thirdBook.style.visibility = 'visible';
+    fourthBook.style.visibility = 'visible';
+
+    fifthBook.style.visibility = 'hidden';
+    sixthBook.style.visibility = 'hidden';
+    seventhBook.style.visibility = 'hidden';
+    eighthBook.style.visibility = 'hidden';
+
+    ninthBook.style.visibility = 'hidden';
+    tenthBook.style.visibility = 'hidden';
+    elevenBook.style.visibility = 'hidden';
+    twelfthBook.style.visibility = 'hidden';
+    
+    thirteenthBook.style.visibility = 'hidden';
+    fourteenthBook.style.visibility = 'hidden';
+    fifteenthBook.style.visibility = 'hidden';
+    sixteenthBook.style.visibility = 'hidden';
+
+
+    firstBook.style.opacity = 1;
+    secondBook.style.opacity = 1;
+    thirdBook.style.opacity = 1;
+    fourthBook.style.opacity = 1;
+
+    fifthBook.style.opacity = 0;
+    sixthBook.style.opacity = 0;
+    seventhBook.style.opacity = 0;
+    eighthBook.style.opacity = 0;
+
+    ninthBook.style.opacity = 0;
+    tenthBook.style.opacity = 0;
+    elevenBook.style.opacity = 0;
+    twelfthBook.style.opacity = 0;
+    
+    thirteenthBook.style.opacity = 0;
+    fourteenthBook.style.opacity = 0;
+    fifteenthBook.style.opacity = 0;
+    sixteenthBook.style.opacity = 0;
+});
+
+document.querySelector('.label2').addEventListener('click', function(){
+    firstBook.style.visibility = 'hidden';
+    secondBook.style.visibility = 'hidden';
+    thirdBook.style.visibility = 'hidden';
+    fourthBook.style.visibility = 'hidden';
+
+    fifthBook.style.visibility = 'visible';
+    sixthBook.style.visibility = 'visible';
+    seventhBook.style.visibility = 'visible';
+    eighthBook.style.visibility = 'visible';
+
+    ninthBook.style.visibility = 'hidden';
+    tenthBook.style.visibility = 'hidden';
+    elevenBook.style.visibility = 'hidden';
+    twelfthBook.style.visibility = 'hidden';
+    
+    thirteenthBook.style.visibility = 'hidden';
+    fourteenthBook.style.visibility = 'hidden';
+    fifteenthBook.style.visibility = 'hidden';
+    sixteenthBook.style.visibility = 'hidden';
+
+
+    firstBook.style.opacity = 0;
+    secondBook.style.opacity = 0;
+    thirdBook.style.opacity = 0;
+    fourthBook.style.opacity = 0;
+
+    fifthBook.style.opacity = 1;
+    sixthBook.style.opacity = 1;
+    seventhBook.style.opacity = 1;
+    eighthBook.style.opacity = 1;
+
+    ninthBook.style.opacity = 0;
+    tenthBook.style.opacity = 0;
+    elevenBook.style.opacity = 0;
+    twelfthBook.style.opacity = 0;
+    
+    thirteenthBook.style.opacity = 0;
+    fourteenthBook.style.opacity = 0;
+    fifteenthBook.style.opacity = 0;
+    sixteenthBook.style.opacity = 0;
+});
+
+document.querySelector('.label3').addEventListener('click', function(){
+    firstBook.style.visibility = 'hidden';
+    secondBook.style.visibility = 'hidden';
+    thirdBook.style.visibility = 'hidden';
+    fourthBook.style.visibility = 'hidden';
+
+    fifthBook.style.visibility = 'hidden';
+    sixthBook.style.visibility = 'hidden';
+    seventhBook.style.visibility = 'hidden';
+    eighthBook.style.visibility = 'hidden';
+
+    ninthBook.style.visibility = 'visible';
+    tenthBook.style.visibility = 'visible';
+    elevenBook.style.visibility = 'visible';
+    twelfthBook.style.visibility = 'visible';
+    
+    thirteenthBook.style.visibility = 'hidden';
+    fourteenthBook.style.visibility = 'hidden';
+    fifteenthBook.style.visibility = 'hidden';
+    sixteenthBook.style.visibility = 'hidden';
+
+
+    firstBook.style.opacity = 0;
+    secondBook.style.opacity = 0;
+    thirdBook.style.opacity = 0;
+    fourthBook.style.opacity = 0;
+
+    fifthBook.style.opacity = 0;
+    sixthBook.style.opacity = 0;
+    seventhBook.style.opacity = 0;
+    eighthBook.style.opacity = 0;
+
+    ninthBook.style.opacity = 1;
+    tenthBook.style.opacity = 1;
+    elevenBook.style.opacity = 1;
+    twelfthBook.style.opacity = 1;
+    
+    thirteenthBook.style.opacity = 0;
+    fourteenthBook.style.opacity = 0;
+    fifteenthBook.style.opacity = 0;
+    sixteenthBook.style.opacity = 0;
+});
+
+document.querySelector('.label4').addEventListener('click', function(){
+    firstBook.style.visibility = 'hidden';
+    secondBook.style.visibility = 'hidden';
+    thirdBook.style.visibility = 'hidden';
+    fourthBook.style.visibility = 'hidden';
+
+    fifthBook.style.visibility = 'hidden';
+    sixthBook.style.visibility = 'hidden';
+    seventhBook.style.visibility = 'hidden';
+    eighthBook.style.visibility = 'hidden';
+
+    ninthBook.style.visibility = 'hidden';
+    tenthBook.style.visibility = 'hidden';
+    elevenBook.style.visibility = 'hidden';
+    twelfthBook.style.visibility = 'hidden';
+    
+    thirteenthBook.style.visibility = 'visible';
+    fourteenthBook.style.visibility = 'visible';
+    fifteenthBook.style.visibility = 'visible';
+    sixteenthBook.style.visibility = 'visible';
+
+
+    firstBook.style.opacity = 0;
+    secondBook.style.opacity = 0;
+    thirdBook.style.opacity = 0;
+    fourthBook.style.opacity = 0;
+
+    fifthBook.style.opacity = 0;
+    sixthBook.style.opacity = 0;
+    seventhBook.style.opacity = 0;
+    eighthBook.style.opacity = 0;
+
+    ninthBook.style.opacity = 0;
+    tenthBook.style.opacity = 0;
+    elevenBook.style.opacity = 0;
+    twelfthBook.style.opacity = 0;
+    
+    thirteenthBook.style.opacity = 1;
+    fourteenthBook.style.opacity = 1;
+    fifteenthBook.style.opacity = 1;
+    sixteenthBook.style.opacity = 1;
+});
