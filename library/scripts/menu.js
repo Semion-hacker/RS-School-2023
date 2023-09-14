8468ff4c4bbea6687e7e929ca25a191090a8a96a
Semion-hacker <obucheniekurs7@gmail.com>
2023-09-14 20:30:34 +0300

    feat: obtained form validation

--- library/scripts/menu.js
+++ library/scripts/menu.js
@@ -4,7 +4,9 @@
     const menuCross = document.querySelector('.nav__cross')
     item.addEventListener('click', () => {
         burgerMenu.classList.add('menu__nav-appears');
-        
+        i = 0;
+        document.querySelector('.dropMenu_Profile_no_Auth').style.visibility = 'hidden';
+        document.querySelector('.dropMenu_Profile_no_Auth').style.opacity = 0;
     });
     menuCross.addEventListener('click', () => {
         burgerMenu.classList.remove('menu__nav-appears');
