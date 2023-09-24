0cd654874e3d6ff0d520303bd6a9f52dc066bdbb
Semion-hacker <obucheniekurs7@gmail.com>
2023-09-24 20:41:21 +0300

    feat: wrote js code

---
+++ audio-player/scripts/index.js
@@ -0,0 +1,104 @@
+const player = document.querySelector('.player'),
+      playBtn = document.querySelector('.play_img'),
+      prevBtn = document.querySelector('.prev_img'),
+      nextBtn = document.querySelector('.next_img'),
+      audio = document.querySelector('.audio'),
+      progressContainer = document.querySelector('.progress_container'),
+      progress = document.querySelector('.progress'),
+      author = document.querySelector('.author'),
+      title = document.querySelector('.song'),
+      background = document.querySelector('.background'),
+      img = document.querySelector('.img')
+
+const songs = ['Ты всегда со мной', 'Обратись, Господи', 'Кто как Ты, Господи'];
+const authorList = ['Simon Khorolskiy', 'Simon K.', 'S. Khorolskiy'];
+
+let songIndex = 0;
+
+function loadSong(song) {
+    title.innerHTML = song;
+    author.innerHTML = authorList[songIndex];
+    audio.src = `audio/${song}.mp3`
+    background.src = `img/png/img${songIndex + 1}.png`
+    img.src = `img/png/img${songIndex + 1}.png`
+}
+
+loadSong(songs[songIndex])
+
+//play
+
+function playSong() {
+    player.classList.add('play')
+    playBtn.src = `img/png/pause.png`;
+    audio.play()
+}
+
+//pause
+
+function pauseSong() {
+    player.classList.remove('play')
+    playBtn.src = `img/png/play.png`;
+    audio.pause()
+}
+
+
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = player.classList.contains('play')
+    if (isPlaying) {
+        pauseSong()  
+    } else {
+        playSong()
+    }
+})
+
+//next song
+
+function nextSong() {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+nextBtn.addEventListener('click', nextSong)
+
+//prev song
+
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+prevBtn.addEventListener('click', prevSong)
+
+//progress bar
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement;
+    const progreesPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progreesPercent}%`;
+}
+
+audio.addEventListener('timeupdate', updateProgress)
+
+// set progress
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+
+progressContainer.addEventListener('click', setProgress)
+
+//autoplay
+audio.addEventListener('ended', nextSong)
